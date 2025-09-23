fb4591a55d740885dab4e26ac33bf0bf3fa51488
mason.zhu <ws_zhu@foxmail.com>
2025-09-23 19:55:42 +0800

    feat: 后端初始化
    1、初始化项目
    2、添加依赖和配置（hutool和knife4接口文档）
    3、提供基础代码（自定义异常、请求响应包装类）
    4、提供基础接口（健康检查接口）

---
+++ src/main/java/com/miu/codemain/AiPageGenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.miu.codemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPageGenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPageGenBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/miu/codemain/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.miu.codemain.controller;
+
+import com.miu.codemain.common.BaseResponse;
+import com.miu.codemain.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
