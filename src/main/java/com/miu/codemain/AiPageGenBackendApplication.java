7490e99482e03abc5b01b34d5e823f0428fc3d0a
mason.zhu <ws_zhu@foxmail.com>
2025-09-25 21:59:44 +0800

    feat: 用户模块 1、数据库初始化 2、解决Long转化为JSON的精度问题 3、用户模块

--- src/main/java/com/miu/codemain/AiPageGenBackendApplication.java
+++ src/main/java/com/miu/codemain/AiPageGenBackendApplication.java
@@ -1,9 +1,11 @@
 package com.miu.codemain;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.miu.codemain.mapper")
 public class AiPageGenBackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/miu/codemain/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.miu.codemain.mapper;
+
+import com.miu.codemain.model.entity.User;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/YeXingKe">野行客</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
